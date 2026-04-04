d4964c44c30f95822b115c35de39a9fe50dbaf0a
slvbush <slvbush@niuitmo.ru>
2026-04-04 16:55:31 +0300

    Delete build directory

--- build/Default-SanitizedDebug/CMakeFiles/tests.dir/test/sched/run-loop-test.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/tests.dir/test/sched/run-loop-test.cpp.o: \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/17/share/asan_ignorelist.txt \
-  /Users/slvbush/Documents/cpp/sorokin_2nd_sem/async-runtime-viacheslav_bushuev/test/sched/run-loop-test.cpp
--- build/Default-SanitizedDebug/CMakeFiles/tests.dir/test/sched/thread-pool-test.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/tests.dir/test/sched/thread-pool-test.cpp.o: \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/17/share/asan_ignorelist.txt \
-  /Users/slvbush/Documents/cpp/sorokin_2nd_sem/async-runtime-viacheslav_bushuev/test/sched/thread-pool-test.cpp
