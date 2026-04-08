f9cd3c364f476bc8dd606f42f3f31d595c9b9a40
Slava Bushuev <slvbush@Slavas-MacBook-Pro.local>
2026-04-08 20:46:32 +0300

    minor changes before lockfree experiment

--- lib/sync/channel.h
+++ lib/sync/channel.h
@@ -17,7 +17,7 @@ template <typename T>
 class BufferedChannel {
 public:
   explicit BufferedChannel(std::size_t capacity)
-      : impl(std::make_shared<State>(capacity)) {}
+      : _channel_state(std::make_shared<State>(capacity)) {}
 
 private:
   struct State;
@@ -33,33 +33,34 @@ private:
     }
   }
 
-  struct Send {};
+  enum class Action {
+    Send,
+    Recv
+  };
 
-  struct Recv {};
-
-  static void move_value_to_buffer(State& state, T&& val) {
-    state.buffer.push(std::move(val));
+  static void move_value_to_buffer(State& state, T&& _val) {
+    state._buffer.push(std::move(_val));
   }
 
-  static void move_value_to_value(T& lhs, T& rhs) {
+  static void move_value_to_value(T&& lhs, T& rhs) {
     std::construct_at(&rhs, std::move(lhs));
   }
 
-  static void move_from_buffer_to_value(auto& state, T& val) {
-    std::construct_at(&val, std::move(state.buffer.front()));
-    state.buffer.pop();
+  static void move_from_buffer_to_value(State& state, T&& _val) {
+    std::construct_at(&_val, std::move(state._buffer.front()));
+    state._buffer.pop();
   }
 
-  template <typename ActionT, typename FirstAwaiterT, typename SecondAwaiterT>
+  template <Action action, typename FirstAwaiterT, typename SecondAwaiterT>
   static bool
-  try_sync(auto& state, FirstAwaiterT& that, SecondAwaiterT*& head, SecondAwaiterT*& tail, bool capacity_valid) {
-    if (capacity_valid || state.capacity == 0) {
+  try_sync(State& state, FirstAwaiterT& that, SecondAwaiterT*& head, SecondAwaiterT*& tail, bool capacity_valid) {
+    if (capacity_valid || state._capacity == 0) {
       if (!head) {
-        if (state.capacity > 0) {
-          if constexpr (std::is_same_v<ActionT, Send>) {
-            move_value_to_buffer(state, std::move(that.val));
+        if (state._capacity > 0) {
+          if constexpr (action == Action::Send) {
+            move_value_to_buffer(state, std::move(that._val));
           } else {
-            move_from_buffer_to_value(state, that.val);
+            move_from_buffer_to_value(state, std::move(that._val));
           }
           return true;
         }
@@ -69,14 +70,14 @@ private:
           tail = nullptr;
         }
         head = head->next;
-        if constexpr (std::is_same_v<ActionT, Send>) {
-          move_value_to_value(that.val, aw->val);
+        if constexpr (action == Action::Send) {
+          move_value_to_value(std::move(that._val), aw->_val);
         } else {
-          if (state.capacity > 0) {
-            move_from_buffer_to_value(state, that.val);
-            move_value_to_buffer(state, std::move(aw->val));
+          if (state._capacity > 0) {
+            move_from_buffer_to_value(state, std::move(that._val));
+            move_value_to_buffer(state, std::move(aw->_val));
           } else {
-            move_value_to_value(aw->val, that.val);
+            move_value_to_value(std::move(aw->_val), that._val);
           }
         }
         coro::Coroutine::current_scheduler()->spawn(aw->awaiting_coroutine.promise());
@@ -88,25 +89,33 @@ private:
 
   class SendAwaiter {
   public:
-    template <typename U = T>
-    SendAwaiter(BufferedChannel& ch, U&& val)
-        : state_ptr(ch.impl)
-        , val(std::forward<U>(val)) {}
+    template <typename U>
+      requires (std::is_same_v<std::remove_cvref_t<U>, T>)
+    SendAwaiter(BufferedChannel& ch, U&& _val)
+        : state_ptr(ch._channel_state)
+        , _val(std::forward<U>(_val)) {}
 
     bool await_ready() {
-      auto& state = *state_ptr;
-      std::scoped_lock lock(state.buffer_mutex, state.receivers_mutex);
-      return try_sync<Send>(state, *this, state.recv_head, state.recv_tail, state.buffer.size() < state.capacity);
+      State& state = *state_ptr;
+      std::scoped_lock lock(state._buffer_mutex, state._receivers_mutex);
+      return try_sync<Action::Send>(
+          state,
+          *this,
+          state._recv_head,
+          state._recv_tail,
+          state._buffer.size() < state._capacity
+      );
     }
 
     bool await_suspend(std::coroutine_handle<coro::Coroutine::promise_type> handle) {
-      auto& state = *state_ptr;
-      std::scoped_lock lock(state.buffer_mutex, state.receivers_mutex, state.senders_mutex);
-      if (try_sync<Send>(state, *this, state.recv_head, state.recv_tail, state.buffer.size() < state.capacity)) {
+      State& state = *state_ptr;
+      std::scoped_lock lock(state._buffer_mutex, state._receivers_mutex, state._senders_mutex);
+      if (try_sync<
+              Action::Send>(state, *this, state._recv_head, state._recv_tail, state._buffer.size() < state._capacity)) {
         return false;
       }
       awaiting_coroutine = handle;
-      enqueue_awaiter(*this, state.send_head, state.send_tail);
+      enqueue_awaiter(*this, state._send_head, state._send_tail);
       return true;
     }
 
@@ -116,44 +125,44 @@ private:
     SendAwaiter* next{nullptr};
     std::shared_ptr<State> state_ptr;
 
-    T val;
+    T _val;
   };
 
   class RecvAwaiter {
   public:
     bool await_ready() {
-      auto& state = *state_ptr;
-      std::scoped_lock lock(state.buffer_mutex, state.senders_mutex);
-      return try_sync<Recv>(state, *this, state.send_head, state.send_tail, state.buffer.size() > 0);
+      State& state = *state_ptr;
+      std::scoped_lock lock(state._buffer_mutex, state._senders_mutex);
+      return try_sync<Action::Recv>(state, *this, state._send_head, state._send_tail, state._buffer.size() > 0);
     }
 
     bool await_suspend(std::coroutine_handle<coro::Coroutine::promise_type> handle) {
-      auto& state = *state_ptr;
-      std::scoped_lock lock(state.buffer_mutex, state.senders_mutex, state.receivers_mutex);
-      if (try_sync<Recv>(state, *this, state.send_head, state.send_tail, state.buffer.size() > 0)) {
+      State& state = *state_ptr;
+      std::scoped_lock lock(state._buffer_mutex, state._senders_mutex, state._receivers_mutex);
+      if (try_sync<Action::Recv>(state, *this, state._send_head, state._send_tail, state._buffer.size() > 0)) {
         return false;
       }
       awaiting_coroutine = handle;
-      enqueue_awaiter(*this, state.recv_head, state.recv_tail);
+      enqueue_awaiter(*this, state._recv_head, state._recv_tail);
       return true;
     }
 
     T await_resume() {
-      return std::move(val);
+      return std::move(_val);
     }
 
   private:
     friend class BufferedChannel;
 
     RecvAwaiter(BufferedChannel& ch)
-        : state_ptr(ch.impl) {}
+        : state_ptr(ch._channel_state) {}
 
     std::coroutine_handle<coro::Coroutine::promise_type> awaiting_coroutine;
     RecvAwaiter* next{nullptr};
     std::shared_ptr<State> state_ptr;
 
     union {
-      T val;
+      T _val;
     };
   };
 
@@ -173,20 +182,20 @@ public:
 private:
   struct State {
     explicit State(std::size_t capacity)
-        : capacity(capacity) {}
+        : _capacity(capacity) {}
 
-    std::size_t capacity;
-    SendAwaiter* send_head{nullptr};
-    SendAwaiter* send_tail{nullptr};
-    RecvAwaiter* recv_head{nullptr};
-    RecvAwaiter* recv_tail{nullptr};
-    std::queue<T> buffer;
-    std::mutex senders_mutex;
-    std::mutex receivers_mutex;
-    std::mutex buffer_mutex;
+    std::size_t _capacity;
+    SendAwaiter* _send_head{nullptr};
+    SendAwaiter* _send_tail{nullptr};
+    RecvAwaiter* _recv_head{nullptr};
+    RecvAwaiter* _recv_tail{nullptr};
+    std::queue<T> _buffer;
+    std::mutex _senders_mutex;
+    std::mutex _receivers_mutex;
+    std::mutex _buffer_mutex;
   };
 
-  std::shared_ptr<State> impl;
+  std::shared_ptr<State> _channel_state;
 };
 
 } // namespace ct::sync
